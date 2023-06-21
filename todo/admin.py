d30396a483d96566f2bfd4b078766a62bbf775ed
selim-haspolat <118964736+selim-haspolat@users.noreply.github.com>
2023-06-21 23:07:20 +0300

    done

--- todo/admin.py
+++ todo/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import (
-    Todo
-)
+from .models import Todo
 
 admin.site.register(Todo)
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.contrib import messages
 from django.shortcuts import redirect
@@ -19,37 +21,97 @@ def todo_list(request):
 # Add:
 def todo_add(request):
     form = TodoForm(request.POST or None)
-
     if form.is_valid():
         form.save()
         # Message:
         messages.success(request, 'Kaydedildi.')
         # If OK redirect:
         return redirect('todo_list') # redirect('path_name')
-
     context = {
         "form": form
     }
-    return render(request, 'add.html', context)
+    # return render(request, 'add.html', context)
+    return render(request, 'add_update.html', context)
 
-
+# Update:
 def todo_update(request, pk):
     todo = Todo.objects.get(id=pk)
     form = TodoForm(instance=todo)
     if request.method== "POST":
-        form = TodoForm(request.POST, instance=todo)
+        form = TodoForm(data=request.POST, instance=todo)
         if form.is_valid():
             form.save()
             messages.success(request, 'Güncellendi.')
-            return redirect("index")
-    return render(request, 'update.html', {
+            return redirect("todo_list")
+    context = {
         'form': form,
         'todo': todo
-    })
+    }
+    # return render(request, 'update.html', context)
+    return render(request, 'add_update.html', context)
 
-
+# Delete:
 def todo_delete(request, pk):
     todo = Todo.objects.get(id=pk)
     todo.delete()
     messages.success(request, 'Silindi.')
-    return redirect("index")
+    # No need template.
+    return redirect("todo_list")
+
+
+# ------------------------------------
+# Class Based Views
+# ------------------------------------
+from django.urls import reverse_lazy
+
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
+
+
+class TodoListView(ListView):
+    model = Todo
+    ordering = ['-id']
+    # template_name = 'todo_list.html' # after 'templates/' default: 'modelname/modelname_list.html'
+
+
+class TodoDetailView(DetailView):
+    model = Todo
+    # template_name = 'todo_detail.html' # after 'templates/' default: 'modelname/modelname_detail.html'
+
+
+class TodoCreateView(CreateView):
+    model = Todo
+    form_class = TodoForm
+    success_url = reverse_lazy('todo_list')
+    # template_name = 'todo_form.html' # after 'templates/' default: 'modelname/modelname_form.html'
+
+    def post(self, request, *args, **kwargs):
+        messages.success(request, 'Kaydedildi.')
+        return super().post(request, *args, **kwargs)
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    form_class = TodoForm
+    success_url = reverse_lazy('todo_list')
+    # template_name = 'todo_form.html' # after 'templates/' default: 'modelname/modelname_form.html'
+
+    def post(self, request, *args, **kwargs):
+        messages.success(request, 'Güncellendi.')
+        return super().post(request, *args, **kwargs)
+
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    # form_class = TodoForm
+    success_url = reverse_lazy('todo_list')
+    # template_name = 'todo_confirm_delete.html' # after 'templates/' default: 'modelname/modelname_confirm_delete.html'
+
+    def post(self, request, *args, **kwargs):
+        messages.success(request, 'Silindi.')
+        return super().post(request, *args, **kwargs)
