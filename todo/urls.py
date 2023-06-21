605463ae93c507acf8237ebace42d7ba76b8d211
selim-haspolat <118964736+selim-haspolat@users.noreply.github.com>
2023-06-21 22:03:23 +0300

    urls

--- todo/urls.py
+++ todo/urls.py
@@ -1,4 +1,17 @@
 from django.urls import path, include
 
+from .views import (
+    # FBV:
+    todo_list,
+    todo_add,
+    todo_update,
+    todo_delete,
+)
+
+# after '/':
 urlpatterns = [
+    path('list/', todo_list, name='todo_list'),
+    path('add/', todo_add,  name='todo_add'),
+    path('update/<int:pk>', todo_update, name="todo_update"),
+    path('delete/<int:pk>', todo_delete, name="todo_delete"),
 ]
